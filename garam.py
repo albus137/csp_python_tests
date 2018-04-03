89eed7cc7c61a079b1b4451e739a4822f39286f9
albus137 <loic.colombi@gmail.com>
2018-04-03 16:04:26 +0200

    Two games solved with CSP: Sudoku and Garam

---
+++ garam.py
@@ -0,0 +1,134 @@
+# modules python-constraint, pyexcel and pyexcel-ods3
+# have been installed
+from constraint import *
+import pyexcel as pe
+from pprint import pprint
+from time import time
+
+# create the problem object
+problem = Problem(RecursiveBacktrackingSolver())
+
+# import the Garam grid
+grid = pe.get_array(file_name='csp_grid.ods')
+n_lines, n_cols = len(grid), len(grid[0])
+
+# display the Garam grid nicely
+for i in range(n_lines):
+    for j in range(n_cols):
+        if grid[i][j] == '':
+            print(' ', end='')
+        else:
+            print(grid[i][j], end='')
+    print()
+
+# add the variables to the problem
+for i in range(n_lines):
+    for j in range(n_cols):
+        if grid[i][j] == '?':
+            domain = range(0, 10)
+        elif type(grid[i][j]) == int:
+            domain = [grid[i][j]]
+
+        problem.addVariable((i, j), domain)
+
+# create the "coor grid" (grid whose cases are tuples containing the cases coordinates)
+grid_coor = [[(i, j) for j in range(n_cols)] for i in range(n_lines)]
+
+# set the first case of each constraint
+h_constraints_origins = [[0, 0], [0, 8],
+                         [2, 4],
+                         [5, 0], [5, 8],
+                         [9, 0], [9, 8],
+                         [11, 4],
+                         [14, 0], [14, 8]]
+
+v_constraints_origins = [[0, 0], [9, 0],
+                         [5, 2],
+                         [0, 4], [9, 4],
+                         [0, 8], [9, 8],
+                         [5, 10],
+                         [0, 12], [9, 12]]
+
+# get the horizontal constraints
+h_constraints = []
+for origin in h_constraints_origins:
+    i_origin, j_origin = origin
+    constraint = grid_coor[i_origin][j_origin:j_origin+5]
+    h_constraints.append(constraint)
+
+# get the vertical constraints
+v_constraints = []
+for k in range(len(v_constraints_origins)):
+    i_origin, j_origin = v_constraints_origins[k]
+
+    if k==2 or k==7:
+        nb_cases = 5
+    else:
+        nb_cases = 6
+
+    constraint = [line[j_origin] for line in grid_coor[i_origin:i_origin+nb_cases]]
+    v_constraints.append(constraint)
+
+# add the constraints to the problem
+## equality constraints
+# for i in range(n_lines):
+    # for j in range(n_cols):
+        # if type(grid[i][j]) == int:
+            # problem.addConstraint(ExactSumConstraint(grid[i][j]), [grid_coor[i][j]])
+
+## other constraints
+constraints = h_constraints + v_constraints
+print(len(constraints))
+
+for constraint in constraints:
+    # get the operation type (+, - or *)
+    i, j = constraint[1]
+    op = grid[i][j]
+
+    if len(constraint) == 5:
+        if op == '+':
+            constraint_function = lambda a, b, c: a+b == c
+        elif op == '-':
+            constraint_function = lambda a, b, c: a-b == c
+        elif op == '*':
+            constraint_function = lambda a, b, c: a*b == c
+
+        problem.addConstraint(constraint_function, (constraint[0], constraint[2], constraint[4]))
+        print('{}{}{}={}'.format(grid[constraint[0][0]][constraint[0][1]], op,
+               grid[constraint[2][0]][constraint[2][1]],
+               grid[constraint[4][0]][constraint[4][1]]))
+    elif len(constraint) == 6:
+        if op == '+':
+            constraint_function = lambda a, b, c, d: a+b == c*10+d
+        elif op == '-':
+            constraint_function = lambda a, b, c, d: a-b == c*10+d
+        elif op == '*':
+            constraint_function = lambda a, b, c, d: a*b == c*10+d
+
+        problem.addConstraint(constraint_function, (constraint[0], constraint[2], constraint[4], constraint[5]))
+
+        a1 = grid[constraint[0][0]][constraint[0][1]]
+        a2 = op
+        a3 = grid[constraint[2][0]][constraint[2][1]]
+        a4 = grid[constraint[4][0]][constraint[4][1]]
+        a5 = grid[constraint[5][0]][constraint[5][1]]
+        print('{}{}{}={}{}'.format(a1, a2, a3, a4, a5))
+print()
+print()
+print(len(problem.__dict__['_constraints']))
+
+# solve the problem
+start = time()
+solution = problem.getSolution()
+end = time()
+print('elapsed: {}'.format(end-start))
+
+for i in range(n_lines):
+    for j in range(n_cols):
+        if grid[i][j] == '?' or type(grid[i][j]) == int:
+            print(solution[(i, j)], end='')
+        elif grid[i][j] == '':
+            print(' ', end='')
+        elif type(grid[i][j]) == str:
+            print(grid[i][j], end='')
+    print()
---
+++ sudoku.py
@@ -0,0 +1,63 @@
+from constraint import *
+from pprint import pprint
+
+# grid definition
+grid = [[5, 3, 0,  0, 7, 0,  0, 0, 0],
+        [6, 0, 0,  1, 9, 5,  0, 0, 0],
+        [0, 9, 8,  0, 0, 0,  0, 6, 0],
+        
+        [8, 0, 0,  0, 6, 0,  0, 0, 3],
+        [4, 0, 0,  8, 0, 3,  0, 0, 1],
+        [7, 0, 0,  0, 2, 0,  0, 0, 6],
+        
+        [0, 6, 0,  0, 0, 0,  2, 8, 0],
+        [0, 0, 0,  4, 1, 9,  0, 0, 5],
+        [0, 0, 0,  0, 8, 0,  0, 7, 9]]
+n_lines, n_cols = 9, 9
+n_squares, size_square = 3, 3
+
+# coordinates grid definition (grid_coor[i][j] is equal to tuple (i, j))
+grid_coor = [[(i, j) for j in range(n_cols)] for i in range(n_lines)]
+
+# problem definition
+problem = Problem()
+
+# variables definition
+for i in range(n_lines):
+    for j in range(n_cols):
+        if grid[i][j] == 0:
+            domain = range(1, 10)
+        else:
+            domain = [grid[i][j]]
+
+        problem.addVariable(grid_coor[i][j], domain)
+
+# constraints on lines
+for i in range(n_lines):
+    coors = [(i, j) for j in range(n_cols)]
+    problem.addConstraint(AllDifferentConstraint(), coors)
+
+# constraints on columns
+for j in range(n_cols):
+    coors = [(i, j) for i in range(n_lines)]
+    problem.addConstraint(AllDifferentConstraint(), coors)
+
+# constraints on 3 by 3 squares
+for I in range(n_squares):
+    for J in range(n_squares):
+        coors = [(i, j) for j in range(J*size_square, (J+1)*size_square)
+                 for i in range(I*size_square, (I+1)*size_square)]
+        problem.addConstraint(AllDifferentConstraint(), coors)
+
+# problem solving
+solution = problem.getSolution()
+
+if solution is None:
+    print('there is no solution')
+else:
+    # fill the grid with the values found
+    for coor in solution:
+        grid[coor[0]][coor[1]] = solution[coor] 
+
+    # display the grid
+    pprint(grid)
